8165bae75260317b69b5fff38a4ce1c8489a2d95
Anais-Linka <Nananoushka@free.fr>
2020-07-09 23:49:07 +0200

    first commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,83 @@
+twitter = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# Combien y a-t-il de handle dans cette array ?
+
+puts "Il y a #{twitter.length} handle dans ce tableau." 
+
+# Quelle est le handle le plus court de cette liste ?
+
+puts "Le handle le plus court est #{twitter.min_by(&:length)}."
+
+#Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère) :
+
+n=0
+ twitter.each do |twitter|
+ 	if twitter.length == 6
+ 		n +=1
+ 	end
+ end
+
+ puts "Il y a #{n} handle de 5 caractères."
+
+#Combien commencent par une majuscule (première lettre juste après le @) ?
+
+up = 0
+
+ twitter.each do |twitter|
+ 	if twitter[1] == twitter[1].upcase
+ 		up += 1
+ 	end
+ end
+
+ puts "Dans ce tableau, #{up} handle qui commencent par une majuscule"
+
+
+#Trie la liste de handle par ordre alphabétique.
+
+puts "Les handle dans l'ordre Alphabétique : #{twitter.sort!}"
+
+#Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+
+puts twitter.sort_by { |word| word.length } 
+
+#Quelle est la position dans l'array de la personne "@epenser" ?
+
+puts "L'email se trouve à la #{twitter.index("@epenser")} ème place"
+
+# Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+ 
+puts "Handle de 1 caractères  : #{twitter.select {|word| word.length == 1}}"
+puts "Handle de 2 caractères  : #{twitter.select {|word| word.length == 2}}"
+puts "Handle de 3 caractères  : #{twitter.select {|word| word.length == 3}}"
+puts "Handle de 4 caractères  : #{twitter.select {|word| word.length == 4}}"
+puts "Handle de 5 caractères  : #{twitter.select {|word| word.length == 5}}"
+puts "Handle de 6 caractères  : #{twitter.select {|word| word.length == 6}}"
+puts "Handle de 7 caractères  : #{twitter.select {|word| word.length == 7}}"
+puts "Handle de 8 caractères  : #{twitter.select {|word| word.length == 8}}"
+puts "Handle de 9 caractères  : #{twitter.select {|word| word.length == 9}}"
+puts "Handle de 10 caractères  : #{twitter.select {|word| word.length == 10}}"
+puts "Handle de 11 caractères  : #{twitter.select {|word| word.length == 11}}"
+puts "Handle de 12 caractères  : #{twitter.select {|word| word.length == 12}}"
+puts "Handle de 13 caractères  : #{twitter.select {|word| word.length == 13}}"
+puts "Handle de 14 caractères  : #{twitter.select {|word| word.length == 14}}"
+puts "Handle de 15 caractères  : #{twitter.select {|word| word.length == 15}}"
+puts "Handle de 16 caractères  : #{twitter.select {|word| word.length == 16}}"
+puts "Handle de 17 caractères  : #{twitter.select {|word| word.length == 17}}"
+puts "Handle de 18 caractères  : #{twitter.select {|word| word.length == 18}}"
+puts "Handle de 19 caractères  : #{twitter.select {|word| word.length == 19}}"
+puts "Handle de 20 caractères  : #{twitter.select {|word| word.length == 20}}"
+puts "Handle de 21 caractères  : #{twitter.select {|word| word.length == 21}}"
+puts "Handle de 22 caractères  : #{twitter.select {|word| word.length == 22}}"
+puts "Handle de 23 caractères  : #{twitter.select {|word| word.length == 23}}"
+puts "Handle de 24 caractères  : #{twitter.select {|word| word.length == 24}}"
+puts "Handle de 25 caractères  : #{twitter.select {|word| word.length == 25}}"
+puts "Handle de 26 caractères  : #{twitter.select {|word| word.length == 26}}"
+puts "Handle de 27 caractères  : #{twitter.select {|word| word.length == 27}}"
+puts "Handle de 28 caractères  : #{twitter.select {|word| word.length == 28}}"
+puts "Handle de 29 caractères  : #{twitter.select {|word| word.length == 29}}"
+puts "Handle de 30 caractères  : #{twitter.select {|word| word.length == 30}}"
+puts "Handle de 31 caractères  : #{twitter.select {|word| word.length == 31}}"
+puts "Handle de 32 caractères  : #{twitter.select {|word| word.length == 32}}"
+puts "Handle de 33 caractères  : #{twitter.select {|word| word.length == 33}}"
+
+ 
